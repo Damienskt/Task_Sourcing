4b783f3e47a2de47a45969d6bc1c5e982b565d57
Teo Wen Zong <wenzongteo@gmail.com>
2017-10-04 00:56:54 +0800

    Solve login bug

--- htdocs/login.php
+++ htdocs/login.php
@@ -2,10 +2,12 @@
 <?php
     session_start();
     // Connect to the database. Please change the password and dbname in the following line accordingly
-        $db     = pg_connect("host=localhost port=5432 dbname=Project1 user=postgres password=root");	
+        $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=cs2102");	
   if (isset($_POST['login'])) { 
 		//$password = password_hash($_POST[Password],PASSWORD_DEFAULT);
-        //$check = pg_query($db, "SELECT pw FROM account WHERE username = '$_POST[Username]'");
+        $check = pg_query($db, "SELECT pw FROM account WHERE username = '$_POST[Username]'");
+		$checkData = pg_num_rows($check);
+		/*
 		$checkPass = pg_query($db, "SELECT username FROM account WHERE username ='$_POST[Username]' AND pw='$_POST[Password]'");
         $checkPass = pg_num_rows($checkPass);
          if($checkPass>0) {
@@ -13,19 +15,24 @@
             header("Location: dashBoard.php");
             exit();       
         }
+		
         else{
             $echo1 = '<p>Wrong password or username, Please sign up or try again!</p>';
         }
-		/*
-		
-		if (password_verify($_POST['Password'],$check[0])) {
-			$_SESSION["user"] = $_POST["Username"];
-            header("Location: http://localhost:8080/demo/dashBoard.php");
-            exit();   
-		} else {
-			$echo1 = '<p>Wrong password $password $checkPass </p>';
-		}
 		*/
+		if ($checkData > 0) {
+			$hashedPw = pg_fetch_row($check); //Should this be a while loop?
+			
+			if (password_verify($_POST['Password'],$hashedPw[0])) {
+				$_SESSION["user"] = $_POST["Username"];
+				header("Location: http://localhost:8080/demo/dashBoard.php");
+				exit();   
+			} else {
+				$echo1 = '<p>Wrong password!</p>';
+			}
+		} else {
+			$echo1 = '<p>Wrong user!</p>';
+		}
     }
 ?> 
 
